2a771732562f0d91b5cf62e30be3e952f083ed3d
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2025-03-30 18:18:00 +0700

    chore: downgrade tailwind from v4 to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,26 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "tailwindcss";
+import tailwindcssNesting from "tailwindcss/nesting";
+import autoprefixer from "autoprefixer";
+
+import * as path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
   vite: {
-    plugins: [tailwindcss()]
-  }
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcssNesting(),
+          tailwindcss({
+            config: path.resolve(import.meta.dirname, "tailwind.config.js"),
+          }),
+          autoprefixer(),
+        ],
+      },
+    },
+  },
 });
